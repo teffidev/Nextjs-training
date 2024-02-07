266d907a8e933763f5722f8d34c46c63849547ee
teffidev <tefyberal09@gmail.com>
2024-02-07 07:22:40 -0500

    Update and delete a user

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -18,3 +18,40 @@ export function GET(
   //*Si es menor a 10
   return NextResponse.json({ id: 1, name: "José" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+
+  if (!body.name) {
+    return NextResponse.json(
+      { error: "El nombre es obligatorio" },
+      { status: 400 }
+    );
+  }
+
+  if (params.id > 10) {
+    return NextResponse.json(
+      { error: "El usuario no existe" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10) {
+    return NextResponse.json(
+      { error: "El usuario no existe" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({});
+}
